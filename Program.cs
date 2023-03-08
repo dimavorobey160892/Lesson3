00f102f3f1e624233ee1ef8562ff0412042f5287
dimavorobey160892 <126515872+dimavorobey160892@users.noreply.github.com>
2023-03-08 16:07:46 +0200

    upload files

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿// See https://aka.ms/new-console-template for more information
+namespace ConditionOperator;
+
+class Program
+{
+    static void Main()
+    {
+        int sum = 0;
+        Console.WriteLine("Input x ...");
+        var x = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("x="+x);
+
+        Console.WriteLine("Input y ...");
+        var y = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("y=" + y);
+
+        if (x == y)
+        {
+            Console.WriteLine("Sum=" + x);
+        }
+        else if (y > x)
+        {
+            WriteSum(x, y);
+        }
+        else
+        {
+            WriteSum(y, x);
+        }
+
+    }
+
+    static void WriteSum(int startNumber, int endNumber)
+    {
+        int sum = startNumber;
+        Console.Write("Sum=" + startNumber);
+        for (var i = startNumber + 1; i <= endNumber; i++)
+        {
+            sum += i;
+            Console.Write("+" + i);
+        }
+        Console.Write("=" + sum);
+    }
+
+}
+
+
